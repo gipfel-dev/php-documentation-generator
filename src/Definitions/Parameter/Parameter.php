0e5280e0fcc09ba3e52d1c49abece3b25884dccd
Alexander Gaal <alexander.gaal@live.at>
2022-05-23 15:39:13 +0200

    add definition contract

--- src/Definitions/Parameter/Parameter.php
+++ src/Definitions/Parameter/Parameter.php
@@ -2,13 +2,23 @@
 
 namespace Gipfel\DocumentationGenerator\Definitions\Parameter;
 
-class Parameter
+use Gipfel\DocumentationGenerator\Contracts\DefinitionContract;
+
+class Parameter implements DefinitionContract
 {
-    protected string $in = 'body';
+    public static function define(
+        string $name,
+        string $description = null,
+        bool $required = false,
+        bool $deprecated = false,
+        bool $allowEmptyValues = false,
+    ): Parameter {
+        return new Parameter($name, $description, $required, $deprecated, $allowEmptyValues);
+    }
 
     public function __construct(
         protected string $name,
-        protected string $description = 'null',
+        protected ?string $description = null,
         protected bool $required = false,
         protected bool $deprecated = false,
         protected bool $allowEmptyValues = false
