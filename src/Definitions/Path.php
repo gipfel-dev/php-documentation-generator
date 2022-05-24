063a9119c4141fd45a1e850188f5ab3fcfd58d2a
Alexander Gaal <alexander.gaal@live.at>
2022-05-24 08:15:18 +0200

    add missing variables to path

--- src/Definitions/Path.php
+++ src/Definitions/Path.php
@@ -8,22 +8,44 @@ class Path
 {
     public static function define(
         string $name,
+        string $summary = null,
+        string $description = null,
         Operation $get = null,
         Operation $post = null,
         Operation $patch = null,
         Operation $put = null,
         Operation $delete = null,
+        Operation $options = null,
+        Operation $head = null,
+        Operation $trace = null
     ): Path {
-        return new Path(name: $name, get: $get, post: $post, patch: $patch, put: $put, delete: $delete);
+        return new Path(
+            name: $name,
+            summary: $summary,
+            description: $description,
+            get: $get,
+            post: $post,
+            patch: $patch,
+            put: $put,
+            delete: $delete,
+            options: $options,
+            head: $head,
+            trace: $trace
+        );
     }
 
     public function __construct(
         protected string $name,
+        protected string $summary,
+        protected string $description,
         protected Operation $get,
         protected Operation $post,
         protected Operation $patch,
         protected Operation $put,
         protected Operation $delete,
+        protected Operation $options,
+        protected Operation $head,
+        protected Operation $trace,
     ) {
     }
 }
